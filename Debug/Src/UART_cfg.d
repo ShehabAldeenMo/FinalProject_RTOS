ce1a2c6f8d3a03c508e7b3dbbfc1e01f234827a4
ShehabAldeenMo <shihab.rifai02@eng-st.cu.edu.eg>
2023-09-28 22:29:51 +0300

    Romve unwanted folders

--- Debug/Src/UART_cfg.d
+++
@@ -1,9 +0,0 @@
-Src/UART_cfg.o: ../Src/UART_cfg.c ../Inc/UART_Interface.h \
- ../Inc/UART_Private.h ../Inc/Std_Types.h ../Inc/Platform_Types.h \
- ../Inc/Bit_Math.h ../Inc/UART_Config.h
-../Inc/UART_Interface.h:
-../Inc/UART_Private.h:
-../Inc/Std_Types.h:
-../Inc/Platform_Types.h:
-../Inc/Bit_Math.h:
-../Inc/UART_Config.h:

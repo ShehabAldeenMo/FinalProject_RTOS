8cf653207a91a7f5c442f82740348579f1f67c41
Ali_Mamdouh <frinfkk@gmail.com>
2023-10-21 17:48:28 +0300

    Fix #24 task2 Documentation of port

--- Src/Port_Prg.c
+++ Src/Port_Prg.c
@@ -35,6 +35,58 @@ SemaphoreHandle_t PORT_SemArryOfPins       = NULL ;
 SemaphoreHandle_t PORT_SemArryOfActivition = NULL ;
 #endif
 
+
+/**
+ * \section Service_Name
+ * Port_GetVersionInfo
+ *
+ * \section Description
+ * Function to Get the sw_major_version, sw_minor_version & sw_patch_version
+ *
+ * \section Req_ID
+ * 0x00
+ *
+ * \section Scope
+ * Public
+ *
+ * \section Re-entrancy
+ * Reentrant
+ *
+ * \section Sync_Async
+ * Synchronous
+ *
+ * \section Parameters
+ * \param[in] None
+ * \param[inout] None
+ * \param[out] VersionInfo 
+ * \return None
+ * 
+ *
+ * \section Rational
+ *  Service to get the version information of this module.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * \section Activity_Diagram
+ *
+ * @startuml 
+ * start
+ * #green:Put ModuleID in elements of versioninfo pointer to structure;
+ * #green:Put Port Major, Minor and Patch versions in elements of versioninfo pointer to structure;
+ * #green:Put VENDOR_ID in elements of versioninfo pointer to structure; 
+ * end
+ *
+ * @enduml
+ *
+ *
+ *
+ *
+ *
+ */
 void    Port_GetVersionInfo  (Std_VersionInfoType* versioninfo){
 	versioninfo->moduleID         = MODULE_ID         ;
 	versioninfo->sw_major_version = PORT_MAJOR_VERSION ;
@@ -44,7 +96,109 @@ void    Port_GetVersionInfo  (Std_VersionInfoType* versioninfo){
 }
 
 
-
+/**
+ * \section Service_Name
+ * Port_VidInit
+ *
+ * \section Description
+ * To initialize all pins with the configured modes that used in Cfg.c.
+ *
+ * \section Service_ID
+ * 0x00
+ *
+ * \section Scope
+ * Public
+ *
+ * \section Re-entrancy
+ * Reentrant
+ *
+ *
+ * \section Sync_Async
+ * Synchronous
+ *
+ * 
+ * \section Parameters
+ * \param[in] None 
+ * \param[inout] None
+ * \param[out] None
+ * \return None
+ * 
+ *
+ * \section Rational
+ *  To initialize all pins with the configured modes.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * \section Activity_Diagram
+ *
+ * @startuml 
+ * start
+ * #yellow:Create three binary semaphores then store them in Port_SemArrOfPorts, Port_SemArryOfPins, Port_SemArryOfActivition handlers **__(FreeRTOS Cfg on)__**; 
+ * #yellow:Give two binary semaphores Port_SemArrOfPins, Port_SemArryOfActivition **__(FreeRTOS Cfg on)__**; 
+ *
+ * while (**i < Port_Num?**)
+ *
+ * switch (**ArrOfPorts[i]?**)
+ *
+ * case ( **condition PortA** )
+ * :Reset all pins of the port;
+ * #green:Set dedicated modes selected in Cfg.c in all pins of the port;
+ * case ( **condition PortB** )
+ * :Reset all pins of the port;
+ * :Set dedicated modes selected in Cfg.c in all pins of the port;
+ *
+ * case ( **condition PortC** )
+ * :Reset all pins of the port;
+ * :Set dedicated modes selected in Cfg.c in all pins of the port;
+ *
+ * endswitch
+ *
+ * :i++; 
+ * endwhile
+ *
+ * #yellow:Give binary semaphores Port_SemArrOfPorts **__(FreeRTOS Cfg on)__**; 
+ *
+ * end
+ *
+ * @enduml
+ *
+ *
+ *
+ * \section Sequence_Diagram
+ *
+ * @startuml
+ *
+ * == FreeRTOS Cfg on ==
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreCreateBinary\nFunction":
+ * note right #green: Create semaphore then return handler to it. 
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreCreateBinary\nFunction":
+ * note right #green: Create semaphore then return handler to it. 
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreGive\nFunction": **Port_SemArryOfPins**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreCreateBinary\nFunction":
+ * note right #green: Create semaphore then return handler to it. 
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreGive\nFunction": **Port_SemArryOfActivition**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * "Port_VidInit\nFunction" -> "xSemaphoreGive\nFunction": **Port_SemArrOfPorts**
+ * note right #green: Gives dedicated semaphore. 
+ * 
+ *
+ * @enduml
+ *
+ *
+ *
+ *
+ */
 void Port_VidInit (void){
 	/* To protect shared resources by semphore
 	 * It's created taken
@@ -100,6 +254,90 @@ void Port_VidInit (void){
 }
 
 
+
+/**
+ * \section Service_Name
+ *  Port_EnumSetterPin
+ *
+ * \section Description
+ * To set some thing in buffer call  Port_EnumSetterPin.
+ *
+ * \section Service_ID
+ * 0x01
+ *
+ * \section Scope
+ * Public
+ *
+ * \section Re-entrancy
+ * Reentrant
+ *
+ *
+ * \section Sync_Async
+ * Synchronous
+ *
+ * 
+ * \section Parameters
+ * \param[in] Port_PinType Copy_ChannelId ,Port_PinModeType Copy_Mode
+ * \param[inout] None
+ * \param[out] None
+ * \return Error_State
+ * 
+ *
+ * \section Rational
+ *  if you want to set some thing in buffer call  Port_EnumSetterPin then Port_VidRunnable will execute the configured data.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * \section Activity_Diagram
+ *
+ * @startuml 
+ * start
+ * if (**Copy_ChannelId(input) in range of Micro pins?**) then (**yes**)
+ * #yellow:Take binary semaphores Port_SemArryOfPins and Port_SemArryOfActivition with time portMAX_DELAY **__(FreeRTOS Cfg on)__**; 	 
+ * #green:put in Array[ChannelId] of **__Pin Buffer__** its modes;
+ * #green:put in Array[ChannelId] of **__Activation Buffer__** that it is ON;
+ * #yellow:Give binary semaphores Port_SemArryOfPins and Port_SemArryOfActivition **__(FreeRTOS Cfg on)__**;  
+ * #blue:return E_OK;  
+ * endif 
+ *
+ * #red:Return E_INVALID_PARAMETER;  
+ *
+ * end
+ *
+ * @enduml
+ *
+ *
+ *
+ * \section Sequence_Diagram
+ *
+ * @startuml
+ *
+ * == FreeRTOS Cfg on ==
+ *
+ * "Port_EnumSetterPin\nFunction" -> "xSemaphoreTake\nFunction": **PORT_SemArryOfPins,portMAX_DELAY**
+ * note right #green: Take dedicated semaphore. 
+ *
+ * "Port_EnumSetterPin\nFunction" -> "xSemaphoreTake\nFunction": **PORT_SemArryOfActivition,portMAX_DELAY**
+ * note right #green: Take dedicated semaphore. 
+ *
+ *
+ * "Port_EnumSetterPin\nFunction" -> "xSemaphoreGive\nFunction": **PORT_SemArryOfPins**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * "Port_EnumSetterPin\nFunction" -> "xSemaphoreGive\nFunction": **PORT_SemArryOfActivition**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * @enduml
+ *
+ *
+ *
+ *
+ */
+
 Error_State       Port_EnumSetterPin(Port_PinType Copy_ChannelId ,Port_PinModeType Copy_Mode){
 	if ( Copy_ChannelId >=Port_A0 && Copy_ChannelId <=Port_C14){
 
@@ -118,6 +356,128 @@ Error_State       Port_EnumSetterPin(Port_PinType Copy_ChannelId ,Port_PinModeTy
 	return E_INVALID_PARAMETER ;
 }
 
+
+
+/**
+ * \section Service_Name
+ * Port_VidRunnable
+ *
+ * \section Description
+ * To execute the desired configuration within runtime "refresh pins".
+ *
+ * \section Service_ID
+ * 0x02
+ *
+ * \section Scope
+ * Public
+ *
+ * \section Re-entrancy
+ * Reentrant
+ *
+ *
+ * \section Sync_Async
+ * Synchronous
+ *
+ * 
+ * \section Parameters
+ * \param[in] None
+ * \param[inout] None
+ * \param[out] None
+ * \return None
+ * 
+ *
+ * \section Rational
+ * To execute the desired configuration.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * \section Activity_Diagram
+ *
+ * @startuml 
+ * start 
+ * #yellow:Take binary semaphores Port_SemArryOfPins and Port_SemArryOfActivition with time portMAX_DELAY **__(FreeRTOS Cfg on)__**; 
+ * :Create variable i and initialize it with 0;  
+ * while(**i < = A15**)
+ * 
+ * if (pin[i] is **__Active__**?) then (**yes**)	 
+ * #green:Set this pin to **__Mode of its buffer__**;
+ * endif 
+ *
+ * :i++;
+ * endwhile	 
+ *
+ * while(**i < = B15**)
+ * 
+ * if (pin[i] is **__Active__**?) then (**yes**)	 
+ * #green:Set this pin to **__Mode of its buffer__**;
+ * endif 
+ *
+ * :i++;
+ * endwhile	 
+ *
+ * while(**i < = C15**)
+ * 
+ * if (pin[i] is **__Active__**?) then (**yes**)	 
+ * #green:Set this pin to **__Mode of its buffer__**;
+ * endif 
+ *
+ * :i++;
+ * endwhile	 
+ *
+ * #yellow:Give binary semaphores Port_SemArryOfPins and Port_SemArryOfActivition **__(FreeRTOS Cfg on)__**;  
+ *
+ * end
+ *
+ * @enduml
+ *
+ *
+ *
+ * \section Sequence_Diagram
+ *
+ * @startuml
+ *
+ * == FreeRTOS Cfg on ==
+ *
+ * "Port_VidRunnable\nFunction" -> "xSemaphoreTake\nFunction": **Port_SemArryOfPins, portMAX_DELAY**
+ * note right #green: Take dedicated semaphore. 
+ *
+ * "Port_VidRunnable\nFunction" -> "xSemaphoreTake\nFunction": **Port_SemArryOfActivition, portMAX_DELAY**
+ * note right #green: Take dedicated semaphore. 
+ *
+ *
+ * == Pins of A is Activated ==
+ *
+ * "Port_VidRunnable\nFunction" -> "Port_SetPinMode\nFunction": **GPIOA,i, Port_ArryOfPins[i]**
+ * note right #blue: Set Mode in the buffer to dedicated pin. 
+ *
+ * == Pins of B is Activated ==
+ *
+ * "Port_VidRunnable\nFunction" -> "Port_SetPinMode\nFunction": **GPIOB,(i & ~(0x10)), Port_ArryOfPins[i]**
+ * note right #blue: Set Mode in the buffer to dedicated pin. 
+ *
+ * == Pins of C is Activated and Out ==
+ *
+ * "Port_VidRunnable\nFunction" -> "Port_SetPinMode\nFunction": **GPIOC,(Port_C13 & ~(0x10)), Port_ArryOfPins[i]**
+ * note right #blue: Set Mode in the buffer to dedicated pin. 
+ *
+ * == FreeRTOS Cfg on ==
+ *
+ * "Port_VidRunnable\nFunction" -> "xSemaphoreGive\nFunction": **Port_SemArryOfPins**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * "Port_VidRunnable\nFunction" -> "xSemaphoreGive\nFunction": **Port_SemArryOfActivition**
+ * note right #green: Gives dedicated semaphore. 
+ *
+ * @enduml
+ *
+ *
+ *
+ *
+ */
 void  Port_VidRunnable (void) {
 	uint8 i = 0 ;
 
@@ -154,6 +514,63 @@ void  Port_VidRunnable (void) {
 
 }
 
+
+/**
+ * \section Service_Name
+ * Port_SetPinMode
+ *
+ * \section Description
+ * To select the type of pin input or output.
+ *
+ * \section Service_ID
+ * 0x03
+ *
+ * \section Scope
+ * Public
+ *
+ * \section Re-entrancy
+ * Reentrant
+ *
+ *
+ * \section Sync_Async
+ * Synchronous
+ *
+ * 
+ * \section Parameters
+ * \param[in] Port_PinType , Port_PinModeType
+ * \param[inout] None
+ * \param[out] None
+ * \return None
+ * 
+ *
+ * \section Rational
+ *  To select the type of pin input or output.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * \section Activity_Diagram
+ *
+ * @startuml 
+ * start 
+ *
+ * if (**Pin <= LOW_PIN?**) then (**yes**)	 
+ * #green:Set Mode of this pin through CRL register;
+ * else (**no**)
+ * #green:Set Mode of this pin through CRL register; 
+ * endif 
+ * end
+ *
+ * @enduml
+ *
+ *
+ *
+ *
+ *
+ */
 /*
   Input  : Port_PinType , Port_PinModeType
   output : void
